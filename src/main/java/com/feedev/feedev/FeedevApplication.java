113ad3163a8760059cf5fc51b68e1359090ffb8b
wnstn819 <wnstn819@naver.com>
2023-11-01 18:05:26 +0900

    ✨ Initail Commit!

---
+++ src/main/java/com/feedev/feedev/FeedevApplication.java
@@ -0,0 +1,13 @@
+package com.feedev.feedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedev/feedev/FeedevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedev.feedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
