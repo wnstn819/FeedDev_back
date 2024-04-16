d78d986f9d2ed8ea5f9dab3fc258dc8bca564f54
wnstn819 <wnstn819@naver.com>
2024-04-17 01:32:57 +0900

    ✨Initail Commit!

---
+++ src/main/java/com/feedDev/FeedDevApplication.java
@@ -0,0 +1,13 @@
+package com.feedDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedDev/FeedDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
