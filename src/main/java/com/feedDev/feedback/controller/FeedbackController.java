809a09564efbe058bd17adcb37e0bb7b34a4b31d
wnstn819 <wnstn819@naver.com>
2024-04-17 18:45:52 +0900

    ✨Feat - DockerFile upload, testController

---
+++ src/main/java/com/feedDev/feedback/controller/FeedbackController.java
@@ -0,0 +1,18 @@
+package com.feedDev.feedback.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feedback")
+@RequiredArgsConstructor
+public class FeedbackController {
+
+    @GetMapping
+    public String test(){
+        return "hello";
+    }
+}
